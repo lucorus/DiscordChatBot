ae0054ab1700b3293061a14c091ee7a0cd5ad81d
lucorus <nikita.hom1977@gmail.com>
2023-08-15 13:23:57 +0400

    second commit

--- bot.py
+++ bot.py
@@ -2,16 +2,10 @@ import discord
 from discord.ext import commands
 import asyncio
 import sqlite3
-from datetime import datetime, timedelta
-
+from datetime import datetime
 import config
 
 intents = discord.Intents.all()
-
-# config = {
-#     'token': 'MTAzNTI0NzI0MTQwMjc4NTg0Mg.Gzm1m3.Tp7sAuu2c9rCO9YbGlqVuAOUt90xucOTTDT0Rk',
-#     'prefix': '!',
-# }
 
 bot = commands.Bot('!', intents=intents)
 points = {}
@@ -22,7 +16,7 @@ c = conn.cursor()
 
 # Создание таблицы
 c.execute('''CREATE TABLE IF NOT EXISTS users
- (user_id INTEGER PRIMARY KEY, points INTEGER, last_message_time INTEGER)''')
+ (user_id INTEGER PRIMARY KEY, points INTEGER, last_message_time INTEGER, payment INTEGER)''')
 
 
 @bot.event
@@ -36,7 +30,7 @@ async def on_message(message):
 
     if user_data is None:
         # Если пользователь не найден, добавляем его в базу данных
-        c.execute("INSERT INTO users VALUES (?, ?, ?)", (message.author.id, 0, int(datetime.utcnow().timestamp())))
+        c.execute("INSERT INTO users VALUES (?, ?, ?, 1)", (message.author.id, 0, int(datetime.utcnow().timestamp())))
     else:
         # Если пользователь найден, проверяем время последнего сообщения
         last_message_time = user_data[2]
@@ -45,7 +39,7 @@ async def on_message(message):
 
         # Если прошло больше минуты, добавляем баллы и обновляем время последнего сообщения
         if time_difference > 60:
-            c.execute("UPDATE users SET points=?, last_message_time=? WHERE user_id=?", (user_data[1] + 1, current_time, message.author.id))
+            c.execute("UPDATE users SET points=?, last_message_time=? WHERE user_id=?", (user_data[1] + user_data[3], current_time, message.author.id))
 
     conn.commit()
     await bot.process_commands(message)
@@ -55,9 +49,10 @@ async def on_message(message):
 @bot.event
 async def on_ready():
     while True:
-        await asyncio.sleep(10)
+        await asyncio.sleep(60)
 
 
+# Получаем количество баллов пользователя
 @bot.command(name='points')
 async def get_user_points(ctx, user: discord.Member=None):
     if not user:
@@ -68,6 +63,42 @@ async def get_user_points(ctx, user: discord.Member=None):
         await ctx.send(f"Количество баллов у {user.mention}: {result[0]}")
     else:
         await ctx.send(f"Количество баллов у {user.mention}: 0")
+
+
+# Получаем количество получаемых баллов за сообщение пользователя
+@bot.command(name='payment')
+async def get_user_payment(ctx, user: discord.Member=None):
+    if not user:
+        user = ctx.author
+    c.execute("SELECT payment FROM users WHERE user_id=?", (user.id,))
+    result = c.fetchone()
+    if result:
+        await ctx.send(f"Количество баллов за сообщение у {user.mention}: {result[0]}")
+    else:
+        await ctx.send(f"Количество баллов за сообщение у {user.mention}: 0")
+
+
+# пользователь может купить предмет, увеличивающий кол-во баллов за сообщение (себе или другому человеку)
+@bot.command(name='buy')
+async def buy(ctx, user: discord.Member=None):
+    if not user:
+        user = ctx.author
+    # берём данные покупающего пользователя
+    c.execute("SELECT * FROM users WHERE user_id=?", (ctx.author.id,))
+    buyer = c.fetchone()
+    # если у покупающего баллов больше, чем нужно (или ровно столько), то он может купить
+    if buyer[1] >= 10:
+
+        # забираем баллы у купившего
+        c.execute("UPDATE users SET points=? WHERE user_id=?",(buyer[1] - 10, ctx.author.id))
+
+        # изменяем кол-во баллов за сообщение пользователю
+        c.execute("SELECT * FROM users WHERE user_id=?", (user.id,))
+        user_data = c.fetchone()
+        c.execute("UPDATE users SET payment=? WHERE user_id=?", (user_data[3] + 1, user.id))
+        await ctx.send(f'{ user.name } теперь получает больше баллов за сообщение!')
+    else:
+        await ctx.send('У вас нет нужного количества баллов')
 
 
 bot.run(config.token)
